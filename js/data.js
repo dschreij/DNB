3331a16372ca634094dbd2118e85f3b61795edbd
Daniel Schreij <dschreij@gmail.com>
2016-09-29 21:08:07 +0200

    First working version.

--- js/data.js
+++ js/data.js
@@ -1,8 +1,70 @@
+/**
+ * Shuffles array in place.
+ * @param {Array} a items The array containing the items.
+ */
+function shuffle(a) {
+    var j, x, i;
+    for (i = a.length; i; i--) {
+        j = Math.floor(Math.random() * i);
+        x = a[i - 1];
+        a[i - 1] = a[j];
+        a[j] = x;
+    }
+}
+
 // Variable to store the data in.
 var articles = [
 {
-	image: "aaa"
+	name: "Chips",
+	image: "img/Chips.jpeg",
+	price: "1,69",
+	category: "vice"
 },{
-	image: "bbb"
+	name: "Apple",
+	image: "img/apple.png",
+	price: "1,99",
+	category: "virtue"
+},{
+	name: "Pineapple",
+	image: "img/pineapple.png",
+	price: "2,99",
+	category: "virtue"
+},{
+	name: "Snickers",
+	image: "img/snickers.png",
+	price: "1,00",
+	category: "vice"
+},{
+	name: "Magnum",
+	image: "img/magnum.png",
+	price: "1,50",
+	category: "vice"
+},{
+	name: "Strawberries",
+	image: "img/strawberries.png",
+	price: "2,49",
+	category: "virtue"
 }
 ];
+
+var payment_conditions = [
+	"contant geld",
+	"pinpas",
+	"credit card",
+	"contactloos betalen",
+];
+
+// Preload images
+for(var i = 0; i < articles.length; i++){
+	var img = new Image();
+	img.src = articles[i].image;
+	articles[i].image = img;
+	$(img).addClass('img-responsive');
+	$(img).addClass('center-block');
+}
+
+// Randomize order
+shuffle(articles);
+
+var payment_condition = payment_conditions[Math.floor(Math.random() * 
+	payment_conditions.length)];
--- js/main.js
+++ js/main.js
@@ -1,20 +1,63 @@
-// Variable to store the data in.
-var articles = [
-{
-	image: "aaa"
-},{
-	image: "bbb"
-}
-];
+var responses = [];
+var article;
+var t_start = performance.now();
 
-$("#article").fadeIn('slow');
+function submit_responses(){
+	console.log(responses);
+}
+
+function present_next_article(){
+	article = articles.shift();
+	// If the article list is empty, the experiment is finished
+	if(article === undefined){
+		$("#article-frame").hide();
+		$("#finished-frame").fadeIn('slow');
+		submit_responses();
+		return;
+	}else{
+		// Show the next article
+		$("#article").html(article.image);
+		$("#price").text(article.price);
+		$("#article").fadeIn('medium', function(){
+			$("#buy").removeClass('disabled');
+			$("#skip").removeClass('disabled');
+			t_start = performance.now();
+		});
+		$("#price-row").fadeIn('medium');
+	}
+}
+
+function prepare_for_next(){
+	//Disable buttons
+	$("#buy").addClass('disabled');
+	$("#skip").addClass('disabled');
+	$("#article").hide();
+	$("#price-row").hide();
+	present_next_article();
+}
 
 $("#buy").click( function(event) {
 	event.preventDefault();
-	alert("Buying!");
+	article.decision_time = Math.round(event.timeStamp - t_start);
+	article.choice = "take";
+	responses.push(article);
+	prepare_for_next();
 });
 
 $("#skip").click( function(event) {
 	event.preventDefault();
-	alert("Skipping");
+	article.decision_time = Math.round(event.timeStamp - t_start);
+	article.choice = "leave";
+	responses.push(article);
+	prepare_for_next();
 });
+
+$("#payment-method").text(payment_condition);
+
+setTimeout(function(){
+	$("#payment-frame").fadeOut('fast', function(){
+		present_next_article();
+		$("#article-frame").fadeIn('fast');
+	});
+}, 2000);
+
